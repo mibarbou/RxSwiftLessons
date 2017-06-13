955c0adb5b7bb6119a3f7ed45f568abbac0f9a4b
Michel Barbou Salvador <mbarbou@paradigmadigital.com>
2017-06-13 14:02:18 +0200

    Finished chapter 17

--- RxSwift_v1/17-creating-custom-reactive-extension/starter/iGifTests/iGifTests.swift
+++ RxSwift_v1/17-creating-custom-reactive-extension/starter/iGifTests/iGifTests.swift
@@ -52,6 +52,38 @@ class iGifTests: XCTestCase {
     super.tearDown()
     OHHTTPStubs.removeAllStubs()
   }
+    
+    func testData() {
+        let observable = URLSession.shared.rx.data(request: self.request)
+        expect(observable.toBlocking().firstOrNil()) != nil
+    }
+    
+    func testString() {
+        let observable = URLSession.shared.rx.string(request: self.request)
+        let string = "{\"array\":[\"foo\",\"bar\"],\"foo\":\"bar\"}"
+        expect(observable.toBlocking().firstOrNil()) == string
+    }
+    
+    func testJSON() {
+        let observable = URLSession.shared.rx.json(request: self.request)
+        let string = "{\"array\":[\"foo\",\"bar\"],\"foo\":\"bar\"}"
+        let json = JSON(data: string.data(using: .utf8)!)
+        expect(observable.toBlocking().firstOrNil()) == json
+    }
+    
+    func testError() {
+        var erroredCorrectly = false
+        let observable = URLSession.shared.rx.json(request: self.errorRequest)
+        do {
+            let _ = try observable.toBlocking().first()
+            assertionFailure()
+        } catch (RxURLSessionError.unknown) {
+            erroredCorrectly = true
+        } catch {
+            assertionFailure()
+        }
+        expect(erroredCorrectly) == true
+    }
   
   
 }
